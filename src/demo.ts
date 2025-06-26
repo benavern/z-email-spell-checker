12db2556bcfad1c5b7c16ca1bd6d300d24e69ede
Benjamin Caradeuc <bencrdc@gmail.com>
2025-06-26 10:39:13 +0200

    add no js solution (id & spell-checker-target attribute)

--- src/demo.ts
+++ src/demo.ts
@@ -1,8 +1,8 @@
 import './z-email-spell-checker'
 import type { ZEmailSpellChecker, MailSuggestion } from './z-email-spell-checker';
 
-const $input = document.querySelector<HTMLInputElement>('#full-example-input');
-const $suggestion = document.querySelector<ZEmailSpellChecker>('#full-example-suggestion');
+const $input = document.querySelector<HTMLInputElement>('#attribute-event__input');
+const $suggestion = document.querySelector<ZEmailSpellChecker>('#attribute-event__suggestion');
 
 $input?.addEventListener('input', (event) => {
     const email = (event.target as HTMLInputElement).value.trim();
--- src/z-email-spell-checker.ts
+++ src/z-email-spell-checker.ts
@@ -9,45 +9,79 @@ export type { MailSuggestion };
 @customElement('z-email-spell-checker')
 export class ZEmailSpellChecker extends LitElement {
     @state()
-    private suggestion: MailSuggestion | undefined;
+    private _suggestion?: MailSuggestion;
 
-    @property({ type: String })
-    public ariaLabel: string = 'Appliquer la suggestion d\'email';
+    @state()
+    private _targetEl?: HTMLInputElement;
 
     @property({ type: String })
     public email: string = '';
+
+    @property({ type: String, attribute: 'aria-label' })
+    public ariaLabel: string = 'Appliquer la suggestion d\'email';
+
+    @property({ type: String, attribute:'spell-checker-target' })
+    public spellCheckerTarget?: string;
+
+    connectedCallback(): void {
+        super.connectedCallback();
+
+        if (this.spellCheckerTarget) {
+            this._targetEl = this.ownerDocument.querySelector(`#${this.spellCheckerTarget}`) ?? undefined;
+
+            if (this._targetEl) {
+                this._targetEl.addEventListener('input', this._onTargetInput.bind(this));
+            }
+        }
+    }
+
+    disconnectedCallback(): void {
+        super.disconnectedCallback();
+
+        this._targetEl?.removeEventListener('input', this._onTargetInput.bind(this));
+    }
 
     // override suggestion state value with a new call to emailSpellChecker.run each time the email property changes
     updated(changedProperties: Map<string | number | symbol, unknown>) {
         if (changedProperties.has('email')) this._onEmailChange();
     }
 
-    // broadcast customEvent to notify the parent component of the suggestion click
-    private _onSuggestionClick() {
-        if (!this.suggestion) return;
+    private _onTargetInput(ev: Event) {
+        this.email = (ev.target as HTMLInputElement).value;
+    }
 
+    private _onEmailChange() {
+        const email = this.email.trim();
+        this._suggestion = email ? emailSpellChecker.run({ email }) : undefined;
+    }
+
+    private _onSuggestionClick() {
+        if (!this._suggestion) return;
+
+        // broadcast customEvent to notify the parent component of the suggestion click
         const suggestionClickEvent = new CustomEvent<MailSuggestion>('suggestion:apply', {
-            detail: this.suggestion,
+            detail: this._suggestion,
             bubbles: true,
             composed: true,
         });
 
         this.dispatchEvent(suggestionClickEvent);
-    }
 
-    private _onEmailChange() {
-        const email = this.email.trim();
-        this.suggestion = email ? emailSpellChecker.run({ email }) : undefined;
+        // if attached to a target, set its value to the suggestion
+        if (this._targetEl) {
+            this._targetEl.value = this._suggestion.full;
+            this._targetEl.dispatchEvent(new Event('input', { bubbles: true }));
+        }
     }
 
     render() {
-        return this.suggestion
+        return this._suggestion
             ? html`
                 <button
                     part="suggestion"
-                    .aria-label=${`${this.ariaLabel}: ${this.suggestion.full}`}
+                    aria-label=${`${this.ariaLabel}: ${this._suggestion.full}`}
                     @click=${this._onSuggestionClick}>
-                    ${this.suggestion.full}
+                    ${this._suggestion.full}
                 </button>
             `
             : nothing;
